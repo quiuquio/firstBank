89eae6bb90317919dc9f2dc9575de0503ecddde8
juan2lv <juan2lv@gmail.com>
2014-11-12 15:19:56 +0800

    passwd encrypting and session example in test.php

---
+++ pass.php
@@ -0,0 +1,92 @@
+<?php 
+function to_hex($data)
+{
+    return strtoupper(bin2hex($data));
+}
+/*
+function getpubkey () {
+	$keypath = "/opt/lampp/keys/server.crt";
+	$key_content = file_get_contents($keypath);
+	$keydetails = openssl_pkey_get_details(openssl_get_publickey($key_content));
+	return $keydetails;
+}
+
+function getpubkeyn ($keydetails) {
+	echo to_hex($keydetails["rsa"]["n"]);
+}
+
+function getpubkeye ($keydetails) {
+	echo to_hex($keydetails["rsa"]["e"]);
+}
+
+function decryptdata ($data) {
+	$keypath = "/opt/lampp/keys/server.key";
+	$key_content = file_get_contents($keypath);
+	$pk = openssl_get_privatekey($key_content);
+	//hexadecimal data
+	$data = pack("H*", $data);
+	if (openssl_private_decrypt($data, $r, $pk)) {
+		return $r;
+	}
+	return "encrypt failed.";
+}
+*/
+/**
+* generates a new private and public key pair
+*/
+class RSA 
+{
+	protected $privKey;
+
+	// public key
+	public $rsa_n;
+	public $rsa_e;
+	
+	public function __construct($alg = "sha512", $keyBits = 4096, $keyType = OPENSSL_KEYTYPE_RSA)
+	{
+		$config = array(
+		    "digest_alg" => $alg,
+		    "private_key_bits" => $keyBits,
+		    "private_key_type" => $keyType,
+		);
+
+		// Create the private and public key
+		$res = openssl_pkey_new($config);
+
+		// Extract the private key from $res to $privKey
+		openssl_pkey_export($res, $this->privKey);
+
+		// get key details
+		$pkeyDetails = openssl_pkey_get_details($res);
+
+		// get public key n & e value
+		$this->rsa_n=to_hex($pkeyDetails["rsa"]["n"]);
+		$this->rsa_e=to_hex($pkeyDetails["rsa"]["e"]);
+	}
+
+	public function serialize() {
+		return serialize([$this->privKey, $this->rsa_n, $this->rsa_e]);
+	}
+
+	public function unserialize($data) {
+		$keydata = unserialize($data);
+		$this->privKey = $keydata[0];
+		$this->rsa_n = $keydata[1];
+		$this->rsa_e = $keydata[2];
+	}
+
+	public function decrypt ($indata) {
+		//hexadecimal data
+		$indata = pack("H*", $indata);
+		// Decrypt the data using the private key and store the results in $decrypted
+		if (openssl_private_decrypt($indata, $decrypted, $this->privKey)) {
+			return $decrypted;
+		}
+		else {
+			return "encrypt failed.";
+		}
+	}
+}
+
+?>
+
---
+++ test.php
@@ -0,0 +1,85 @@
+<?php
+require_once('pass.php');
+
+session_start();
+if (!isset($_SESSION["active"])) {
+	$_SESSION["active"] = time();
+	$key = new RSA();
+	$_SESSION["skey"] = $key;
+}
+elseif ((time()-$_SESSION["active"]) < 20) {
+	$_SESSION["active"] = time();
+	$key = $_SESSION["skey"];
+}
+else {
+	session_destroy();
+	$key = NULL;
+}
+
+
+
+$encrypted = "";
+$output = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$encrypted = $_POST["instr"];
+	if ($key==NULL) {
+		$output = "session timeout";
+	}
+	else {
+		$output = $key->decrypt($encrypted);
+	}
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<script type="text/javascript" src="js/jsbn.js"></script>
+	<script type="text/javascript" src="js/rng.js"></script>
+	<script type="text/javascript" src="js/prng4.js"></script>
+	<script type="text/javascript" src="js/rsa.js"></script>
+	<script type="text/javascript">
+		
+		function encryptdata() {
+			var keyn = "<?php echo ($key==NULL)? 0:$key->rsa_n; ?>";
+			var keye = "<?php echo ($key==NULL)? 0:$key->rsa_e; ?>";
+
+			if (keyn == "0") {
+				document.getElementById("instr").value = "";
+			}
+			else {
+				var rsa = new RSAKey();
+				rsa.setPublic(keyn, keye);
+
+				var data = document.getElementById("instr").value;
+
+				var edata = rsa.encrypt(data);
+
+				document.getElementById("instr").value = edata;
+			}
+		}
+	</script>
+</head>
+<body>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" onsubmit="return encryptdata()"> 
+	<input type="text" name="instr" id="instr" value="Hello World">
+	<p>
+		<input type="submit" name="submit" value="Submit">
+	</p>
+</form>
+
+
+<p><?php echo $encrypted;?></p>
+<p><?php echo $output;?></p>
+
+<script>
+var x = "Hello World!";              
+
+document.getElementById("demo").innerHTML =
+typeof x + " " + typeof y;
+</script>
+
+</body>
+</html>
