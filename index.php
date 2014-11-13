d6e404027a090a564f684a28e524d918580f0122
Robert Parcus <betoparcus@gmail.com>
2014-11-13 18:11:47 +0800

    Added facial test code + structural css

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 session_start();
 session_unset();
+echo '<link rel="stylesheet" type="text/css" href="styles/main.css">';
+echo "<div id='wrapper'>";
 require('header.php');
+require('leftColumn.php');
 require('main.php');
+require('rightColumn.php');
 require('footer.php');
+echo "</div>";
 ?>
--- main.php
+++ main.php
@@ -2,8 +2,7 @@
 /*
 if session user is not set we show the login page
 */
+echo "<main>";
 require('login.php');
-
-
-
+echo "</main>";
 ?>
